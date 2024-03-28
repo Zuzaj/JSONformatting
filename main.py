a1c3cdc465f004d09d072720c90f7eb7ad5e9822
Zuzia <zuzia.jarl@gmail.com>
2024-03-28 10:27:00 +0100

    updated conditions

--- main.py
+++ main.py
@@ -4,53 +4,28 @@ import re
 import sys
 
 #Function to check if Resource field consists of exactly 1 asterisk
-def check_resource(filePath: str) -> bool:
-        if (verify_format(filePath)):  #file format verification
+def check_resource(filePath:str) -> bool:
+        if (verify_format(filePath) == True):  #file format verification
             with open(filePath) as f:
                 data = json.load(f)
-                resourceValue = data["PolicyDocument"]["Statement"][0]["Resource"]
-                if(resourceValue) == '*':
+                resourceValues = [data["PolicyDocument"]["Statement"][i]["Resource"] for i in range(len(data["PolicyDocument"]["Statement"]))]
+                if '*' in resourceValues:
                     return False
                 else:
                     return True
+        else:
+            return verify_format(filePath)
 
 #Function to check format correctness     
 def verify_format(filePath: str) -> bool:
-    with open(filePath) as f:
-        try:
-            data = json.load(f)
-            try:
-                conditions =[
-                data.keys() == format_template.keys(), 
-                type(data["PolicyName"]) == str, 
-                re.match('[\w+=,.@-]+', data["PolicyName"]),
-                type(data["PolicyDocument"]) == dict,
-                data["PolicyDocument"].keys() == format_template["PolicyDocument"].keys(),
-                type(data['PolicyDocument']["Statement"]) == type(format_template['PolicyDocument']["Statement"]),
-                data['PolicyDocument']["Statement"][0].keys() == format_template['PolicyDocument']["Statement"][0].keys(),
-                type(data['PolicyDocument']["Statement"][0]["Action"]) == type(format_template['PolicyDocument']["Statement"][0]["Action"]),
-                type(data['PolicyDocument']["Statement"][0]["Sid"]) == type(format_template['PolicyDocument']["Statement"][0]["Sid"]),
-                type(data['PolicyDocument']["Statement"][0]["Effect"]) == type(format_template['PolicyDocument']["Statement"][0]["Effect"]),
-                type(data['PolicyDocument']["Statement"][0]["Resource"]) == type(format_template['PolicyDocument']["Statement"][0]["Resource"])
-                ]
-                if all(conditions):
-                    return True
-                else:
-                    print("Input not in AWS::IAM::Role Policy format") 
-            except:
-                print("Input not in AWS::IAM::Role Policy format")    
-        except:
-            print("File format other than .JSON")   
-    return False
 
-
-#AWS::IAM::Role Policy format template
-format_template = {
-    "PolicyName": "root",
-    "PolicyDocument": {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
+    #AWS::IAM::Role Policy format template
+    format_template = {
+        "PolicyName": "root",
+        "PolicyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
                 "Sid": "IamListAccess",
                 "Effect": "Allow",
                 "Action": [
@@ -58,20 +33,54 @@ format_template = {
                     "iam:ListUsers"
                 ],
                 "Resource": "*"
-            }
-        ]
+                }
+            ]
+        }
     }
-}
+ 
+    with open(filePath) as f:
+        try:
+            data = json.load(f)
+            try:   
+                conditions = [
+                data.keys() == format_template.keys(),
+                type(data["PolicyName"]) == str, 
+                re.match('[a-zA-Z0-9_+=,.@-]+', data["PolicyName"]),
+                len(data["PolicyName"]) >= 1 and len(data["PolicyName"]) <= 128,
+                type(data["PolicyDocument"]) == dict,
+                data["PolicyDocument"].keys() == format_template["PolicyDocument"].keys(),
+                type(data['PolicyDocument']["Statement"]) == list,
+                all([type(data['PolicyDocument']["Statement"][i]) == dict for i in range(len(data['PolicyDocument']["Statement"]))]),
+                all([set(data['PolicyDocument']["Statement"][i].keys()).issubset(("Sid", "Effect", "Principal", "Action", "Resource", "Condition")) for i in range(len(data['PolicyDocument']["Statement"]))]),
+                all(["Effect" in data['PolicyDocument']["Statement"][i] and "Action" in data['PolicyDocument']["Statement"][i] for i in range(len(data['PolicyDocument']["Statement"]))]),
+                all([type(data['PolicyDocument']["Statement"][i]["Action"]) in [list, str] for i in range(len(data['PolicyDocument']["Statement"]))]), 
+                all([data['PolicyDocument']["Statement"][i]["Effect"] in ["Allow", "Deny"] for i in range(len(data['PolicyDocument']["Statement"]))]),
+                all([type(data['PolicyDocument']["Statement"][i]["Resource"]) in [list, str] for i in range(len(data['PolicyDocument']["Statement"]))])
+                 ]
+                if all(conditions):
+                    return True
+                else:
+                    return "Input not in AWS::IAM::Role Policy format"
+            except:
+                return "Input not in AWS::IAM::Role Policy format"   
+        except:
+            return "Input not in .json file format"   
+
+
+
 
 
 def main(argv):
-    if len(argv) < 2 or argv[1][-5:] != ".json":
+    try:
+        input_file = argv
+    except:
         raise ValueError("Pass *.json file as parameter")
-    input_file = argv[1]
     print(check_resource(input_file))
 
 
 
-if __name__ == '__main__':
-    main(sys.argv)
+# if __name__ == '__main__':
+#     main(sys.argv)
 
+
+main("tests/testFiles/testFile2.json")
